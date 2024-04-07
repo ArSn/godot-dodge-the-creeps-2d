428aa892a353c64b4b4e5a42b7681bdcb78346ac
Kolja Zuelsdorf <koljaz@web.de>
2024-04-07 19:40:09 +0200

    Added test to verify that the mob is freeing itself when exiting the screen.

--- test/unit/test_mob.gd
+++ test/unit/test_mob.gd
@@ -10,6 +10,10 @@ func test_mob_plays_sprite_animation():
 	assert_true(sprite.is_playing(), "Animated sprite should be playing")
 
 
-
-func test_do_something_effe():
-	assert_true(true, "This should pass")
+func test_queues_free_when_visible_on_screen_notifier_exits():
+	var mob = TestMob.instantiate()
+	add_child_autoqfree(mob)
+	
+	mob._on_visible_on_screen_notifier_2d_screen_exited()
+	
+	assert_true(mob.is_queued_for_deletion(), "Mob should be queued for deletion")
