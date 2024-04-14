d4de879b11d3c6751076b5cd9d5756baa1ace33e
Kolja Zuelsdorf <koljaz@web.de>
2024-04-14 21:55:25 +0200

    Added tests around the animation playing when the player is moving.

--- test/unit/test_player.gd
+++ test/unit/test_player.gd
@@ -47,3 +47,29 @@ func test_move_up_changes_position_up():
 	await(_sender.idle)
 
 	assert_lt(player.position.y, previous_y, "Player should move up")
+	
+func test_play_animation_when_moving():
+	var player = add_child_autofree(Player.instantiate())
+	var sprite: AnimatedSprite2D = player.get_node("AnimatedSprite2D")
+
+	_sender.action_down("move_right").wait_frames(30)
+	await wait_frames(10)
+	assert_true(sprite.is_playing(), "Player should play animation when moving")
+	await(_sender.idle)	
+
+func test_stop_animation_when_not_moving():
+	var player = add_child_autofree(Player.instantiate())
+	var sprite: AnimatedSprite2D = player.get_node("AnimatedSprite2D")
+	assert_false(sprite.is_playing(), "Player should stop animation when not moving")
+	
+func test_stop_animation_when_having_stopped():
+	var player = add_child_autofree(Player.instantiate())
+	var sprite: AnimatedSprite2D = player.get_node("AnimatedSprite2D")
+
+	# We start and stop the movement for this test
+	_sender.action_down("move_right").wait_frames(10)
+	await(_sender.idle)
+	_sender.action_up("move_right").wait_frames(10)
+	await(_sender.idle)
+	assert_false(sprite.is_playing(), "Player should stop animation after stopping")
+	
