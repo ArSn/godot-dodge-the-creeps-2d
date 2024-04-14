abfb259702d93d041ab6e2e6da4fcb2880f92366
Kolja Zuelsdorf <koljaz@web.de>
2024-04-14 21:41:22 +0200

    Added remainder of the player moving around tests.

--- test/unit/test_player.gd
+++ test/unit/test_player.gd
@@ -8,21 +8,42 @@ func after_each():
 	_sender.release_all()
 	_sender.clear()
 
-func test_move_right_adds_positive_x_velocity():
+func test_move_right_changes_position_right():
 	var player = add_child_autofree(Player.instantiate())
 	var previous_x: float = player.position.x
 
-	_sender.action_down("move_right").wait_frames(1)
+	_sender.action_down("move_right").wait_frames(10)
 	await(_sender.idle)
 	
 	assert_gt(player.position.x, previous_x, "Player should move right")
 	
-#func test_move_left_adds_negative_x_velocity():
-#	var player = add_child_autofree(Player.instantiate())
-#	player.position = Vector2(1, 1)
-#
-#	_sender.action_down("move_left").wait_frames(1)
-#	await(_sender.idle)
-#	
-#	assert_lt(player.position.x, 0.0, "Player should move left")
+func test_move_left_changes_position_left():
+	var player = add_child_autofree(Player.instantiate())
+	# We can't move below 0 so we have to move the player to the right first
+	player.position.x = 100
+	var previous_x: float = player.position.x
+
+	_sender.action_down("move_left").wait_frames(10)
+	await(_sender.idle)
+
+	assert_lt(player.position.x, previous_x, "Player should move left")
+
+func test_move_down_changes_position_down():
+	var player = add_child_autofree(Player.instantiate())
+	var previous_y: float = player.position.y
+
+	_sender.action_down("move_down").wait_frames(10)
+	await(_sender.idle)
+
+	assert_gt(player.position.y, previous_y, "Player should move down")
 	
+func test_move_up_changes_position_up():
+	var player = add_child_autofree(Player.instantiate())
+	# We can't move below 0 so we have to move the player down first
+	player.position.y = 100
+	var previous_y: float = player.position.y
+
+	_sender.action_down("move_up").wait_frames(10)
+	await(_sender.idle)
+
+	assert_lt(player.position.y, previous_y, "Player should move up")
