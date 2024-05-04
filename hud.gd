ed7d2edb10aff9d7182137503ba505844e593acd
Kolja Zuelsdorf <koljaz@web.de>
2024-05-04 22:07:30 +0200

    Completed tests for HUD.

--- hud.gd
+++ hud.gd
@@ -14,10 +14,8 @@ func show_game_over():
 	# Wait until the MessageTimer has counted down.
 	await $MessageTimer.timeout
 
-	$Message.text = "Dodge the Creeps!"
+	$Message.text = "Dodge the creeps!"
 	$Message.show()
-	# Make a one-shot timer and wait for it to finish.
-	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
 
 func update_score(score):
--- test/unit/test_hud.gd
+++ test/unit/test_hud.gd
@@ -35,7 +35,7 @@ func test_on_start_button_pressed_starts_game():
 	assert_false(startButton.visible, "Start button should be hidden")
 	
 func test_on_message_timer_timeout_hides_message():
-	var hud = TestHud.instantiate()
+	var hud = TestHud.instantiate() 
 	autoqfree(hud)
 	
 	var label: Label = hud.get_node("Message")
@@ -45,3 +45,30 @@ func test_on_message_timer_timeout_hides_message():
 	hud._on_message_timer_timeout()
 	
 	assert_false(label.visible, "Message should be hidden")
+
+func test_show_game_over_shows_message_and_then_shows_start_button():
+	var hud = TestHud.instantiate()
+	add_child_autoqfree(hud)
+	
+	# create mock timer that times out almost immediately
+	var timer: Timer = Timer.new()
+	timer.set_wait_time(0.1)
+	timer.name = "MessageTimer"
+	var realTimer: Timer = hud.get_node("MessageTimer")
+	autoqfree(realTimer)
+	hud.get_node("MessageTimer").replace_by(timer)
+	
+	
+	# act
+	hud.show_game_over()
+	
+	# assert
+	var message: Label = hud.get_node("Message")
+	assert_eq(message.text, "Game Over", "Label text should be 'Game Over'")
+
+	timer.start()	
+	await wait_seconds(0.2)
+
+	assert_eq(message.text, "Dodge the creeps!", "Label text should be 'Dodge the creeps!'")
+	var startButton: Button = hud.get_node("StartButton")
+	assert_true(startButton.visible, "Start button should be visible")
