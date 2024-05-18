3b204d14c0614c6b184ce337a3d77f7491b58738
Kolja Zuelsdorf <koljaz@web.de>
2024-05-18 20:52:38 +0200

    Added first test for main script.

---
+++ test/unit/test_main.gd
@@ -0,0 +1,23 @@
+﻿extends GutTest
+
+var TestMain: PackedScene = load('res://main.tscn')
+
+func test_new_game_starts_new_game():
+	var main = TestMain.instantiate()
+	add_child_autoqfree(main)
+
+	# Mock the player
+	var Player = load('res://player.gd')
+	var PlayerDouble = double(Player)
+	var player_double = PlayerDouble.new()
+	player_double.set_name("Player")
+
+	main.get_node("Player").free()
+	main.add_child(player_double)
+	
+	main.new_game()
+	assert_eq(main.score, 0, "Score should be 0")
+	assert_call_count(player_double, "start", 1)
+	
+
+	
