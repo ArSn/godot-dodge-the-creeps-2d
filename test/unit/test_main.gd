c853d166b051761d3f74fb1c8be2fb8a142116d2
Kolja Zuelsdorf <koljaz@web.de>
2024-06-10 21:44:13 +0200

    Created test for the main.game_over() method.

--- test/unit/test_main.gd
+++ test/unit/test_main.gd
@@ -47,6 +47,32 @@ func test_new_game_starts_new_game():
 	# Verify the mobs have been cleared out
 	assert_true( get_tree().get_nodes_in_group("mobs").size() == 0, "There should be 2 mobs" )
 	
+func test_game_over_stops_timers_and_shows_game_over_hud():
+	var main = TestMain.instantiate()
+	add_child_autoqfree(main)
+	
+	# Start the times so we can ensure we stopped them again
+	main.get_node("ScoreTimer").start()
+	main.get_node("MobTimer").start()
+	
+	# Mock the HUD
+	var Hud = load('res://hud.gd')
+	var HudDouble = double(Hud)
+	var hud_double = HudDouble.new()
+	hud_double.set_name("HUD")
+	main.get_node("HUD").free()
+	main.add_child(hud_double)
+	
+	# Act
+	main.game_over()
+	
+	# Verify things are called
+	assert_call_count(hud_double, "show_game_over", 1)
+	# Verify timers are stopped
+	assert_true( main.get_node("ScoreTimer").is_stopped() )
+	assert_true( main.get_node("MobTimer").is_stopped() )
+
+	
 	
 
 	
